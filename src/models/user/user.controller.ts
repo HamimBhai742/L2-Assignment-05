feb93ca201aea0b29f4af2c17980da076c60638e
HamimBhai742 <omagootruelove147@gmail.com>
2025-08-20 09:42:31 +0600

    Create userController

---
+++ src/models/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { createAsyncFunction } from '../../utils/create.asyncFunction';
+import { sendResponse } from '../../utils/send.response';
+import { userServices } from './user.service';
+import httpStatusCode from 'http-status-codes';
+const createUser = createAsyncFunction(async (req: Request, res: Response) => {
+  const data = await userServices.createUser(req.body);
+
+  //send response
+  sendResponse(res, {
+    statusCode: httpStatusCode.CREATED,
+    success: true,
+    message: data.message,
+    data: data.user,
+  });
+});
+
+const userController = {
+  createUser,
+};
--- src/models/user/user.service.ts
+++ src/models/user/user.service.ts
@@ -2,16 +2,20 @@ import { IUser } from './user.interface';
 import { User } from './user.model';
 import bcryptjs from 'bcrypt';
 const createUser = async (payload: Partial<IUser>) => {
-  const { password, email, phone, ...reset } = payload;
+  const { password, email, phone, role, ...reset } = payload;
   const hashPassword = await bcryptjs.hash(password as string, 10);
 
   const user = await User.create({
     email,
     phone,
     password: hashPassword,
+    role,
     ...reset,
   });
-  return user;
+  return {
+    user,
+    message: `${role ? role : 'User'} created successfully`,
+  };
 };
 
 export const userServices = {
